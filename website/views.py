ddc389f0163f35894e6312b00ec981bb0cf6ee6b
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-05-11 22:36:08 +0100

    install and add Jinja templating

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
 # Stores non-authy routes for the website
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
 def home():
-    return "<h1>Hello, World!</h1>"
+    return render_template("home.html")
