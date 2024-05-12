41c259c0286609221d3034682a6ba9dc65755bbc
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-05-12 21:06:29 +0100

    Initialise database

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,11 @@ from dotenv import load_dotenv
 import os
 from .views import views
 from .auth import auth
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 load_dotenv()
 secret = os.getenv("SESSION_COOKIE_SECRET")
@@ -11,6 +16,8 @@ secret = os.getenv("SESSION_COOKIE_SECRET")
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = secret
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     # Registering the blueprints
     app.register_blueprint(views, url_prefix="/")  # /views/
