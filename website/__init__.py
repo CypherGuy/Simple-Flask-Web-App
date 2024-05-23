426494929d20a703725d90a82420feca4b4ddaeb
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-05-23 08:05:42 +0100

    chore: Refactor database initialization and configuration

--- website/__init__.py
+++ website/__init__.py
@@ -1,26 +1,27 @@
 from flask import Flask
-from dotenv import load_dotenv
-import os
-from .views import views
-from .auth import auth
 from flask_sqlalchemy import SQLAlchemy
-
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-load_dotenv()
-secret = os.getenv("SESSION_COOKIE_SECRET")
-
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = secret
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
-    # Registering the blueprints
-    app.register_blueprint(views, url_prefix="/")  # /views/
-    app.register_blueprint(auth, url_prefix="/")  # /auth/
+    from .views import views
+    from .auth import auth
 
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from . import models
+
+    with app.app_context():
+        db.create_all()
+
+    print('Created Database!')
     return app
